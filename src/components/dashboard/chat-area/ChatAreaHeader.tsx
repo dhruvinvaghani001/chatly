ee16af3bbf7b190b6319aaa11bdebaf86568e569
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-10-05 12:25:16 +0530

    fix:leave group chat

--- src/components/dashboard/chat-area/ChatAreaHeader.tsx
+++ src/components/dashboard/chat-area/ChatAreaHeader.tsx
@@ -1,91 +1,120 @@
-import { Button } from '@/components/ui/button'
-import DialogPopUp from '@/components/ui/dialog-popup'
-import { useAuthContext } from '@/context/authSlice'
-import { setSelectedChat, useChatContext } from '@/context/chatSlice'
-import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
-import { Info, Loader2, Trash } from 'lucide-react'
-import React, { useState } from 'react'
-import { requestHandler } from '@/lib/requestHandler'
-import { deleteChats } from '@/api'
-import { useDispatch } from 'react-redux'
-import toast from 'react-hot-toast'
-import GroupChatDetail from './group-chat-detail/GroupChatDetail'
-import { DeleteAlertDialogDemo } from '@/components/ui/delete-alert-dialog'
-import { GroupThumbnail } from '@/assets'
+import { Button } from "@/components/ui/button";
+import DialogPopUp from "@/components/ui/dialog-popup";
+import { useAuthContext } from "@/context/authSlice";
+import { setSelectedChat, useChatContext } from "@/context/chatSlice";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Info, Loader2, LogOut, Trash } from "lucide-react";
+import React, { useState } from "react";
+import { requestHandler } from "@/lib/requestHandler";
+import { deleteChats, leaveGroup } from "@/api";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+import GroupChatDetail from "./group-chat-detail/GroupChatDetail";
+import { DeleteAlertDialogDemo } from "@/components/ui/delete-alert-dialog";
+import { GroupThumbnail } from "@/assets";
 
 const ChatAreaHeader = () => {
-  const { userData } = useAuthContext()
-  const { selectedChat } = useChatContext()
-  const [loading, setLoading] = useState(false)
-  const dispatch = useDispatch()
+  const { userData } = useAuthContext();
+  const { selectedChat } = useChatContext();
+  const [loading, setLoading] = useState(false);
+  const [leaving, setLeaving] = useState(false);
+  const dispatch = useDispatch();
 
   const one2oneMemebr = selectedChat?.members.filter(
-    item => item.username != userData.username
-  )[0]
+    (item) => item.username != userData.username
+  )[0];
 
-  const a = 1
+  const a = 1;
 
   const handleDeleteChat = async () => {
     requestHandler(
       async () =>
         await deleteChats({
           type: false,
-          chatId: selectedChat?._id
+          chatId: selectedChat?._id,
         }),
       setLoading,
-      res => {
-        const { data } = res
+      (res) => {
+        const { data } = res;
         // console.log(data)
-        dispatch(setSelectedChat({ chat: null }))
-        toast.success(res?.message)
+        dispatch(setSelectedChat({ chat: null }));
+        toast.success(res?.message);
       },
-      err => {
-        toast.error(err.message)
+      (err) => {
+        toast.error(err.message);
       }
-    )
-  }
+    );
+  };
+
+  const handleLeaveChat = () => {
+    requestHandler(
+      async () => await leaveGroup(selectedChat._id),
+      setLeaving,
+      (res) => {
+        const { data } = res;
+        // console.log(data);
+        toast.success(res.message);
+      },
+      (err) => {
+        toast.error(err);
+      }
+    );
+  };
 
   return (
     <div>
-      <div className='flex items-center justify-between p-4 border-b border-gray-700'>
-        <div className='flex items-center'>
+      <div className="flex items-center justify-between p-4 border-b border-gray-700">
+        <div className="flex items-center">
           {selectedChat.isGroup ? (
-            <Avatar className='w-10 h-10 mr-3 bg-muted-foreground flex justify-center items-center rounded-full'>
+            <Avatar className="w-10 h-10 mr-3 bg-muted-foreground flex justify-center items-center rounded-full">
               <AvatarImage src={GroupThumbnail}></AvatarImage>
               <AvatarFallback></AvatarFallback>
             </Avatar>
           ) : (
-            <Avatar className='w-10 h-10'>
+            <Avatar className="w-10 h-10">
               <AvatarImage src={one2oneMemebr.avatar}></AvatarImage>
               <AvatarFallback></AvatarFallback>
             </Avatar>
           )}
           <div>
-            <h2 className='font-semibold'>
+            <h2 className="font-semibold">
               {selectedChat?.isGroup
                 ? selectedChat?.name
                 : one2oneMemebr?.username}
             </h2>
           </div>
         </div>
-        <div className='flex space-x-2'>
+        <div className="flex space-x-2">
           {selectedChat.isGroup ? (
-            <DialogPopUp
-              TriggerComponent={
-                <Button variant='ghost' size='icon'>
-                  <Info className='h-5 w-5' />
-                </Button>
-              }
-              ContentCompnent={GroupChatDetail}
-            />
+            <>
+              {selectedChat.admin != userData._id && (
+                <DeleteAlertDialogDemo
+                  triggerButton={
+                    <Button variant="ghost" size="icon">
+                      <LogOut className="h-5 w-5 text-destructive" />
+                    </Button>
+                  }
+                  handleSubmit={handleLeaveChat}
+                  loading={leaving}
+                />
+              )}
+              <DialogPopUp
+                TriggerComponent={
+                  <Button variant="ghost" size="icon">
+                    <Info className="h-5 w-5" />
+                  </Button>
+                }
+                ContentCompnent={GroupChatDetail}
+              />
+            </>
           ) : (
             <DeleteAlertDialogDemo
               triggerButton={
-                <Button variant={'destructive'} size='icon' disabled={loading}>
+                <Button variant={"destructive"} size="icon" disabled={loading}>
                   {loading ? (
-                    <Loader2 className='animte-spin' />
+                    <Loader2 className="animte-spin" />
                   ) : (
-                    <Trash className='h-5 w-6' />
+                    <Trash className="h-5 w-6" />
                   )}
                 </Button>
               }
@@ -96,7 +125,7 @@ const ChatAreaHeader = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatAreaHeader
+export default ChatAreaHeader;
