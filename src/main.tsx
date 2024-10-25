a83f53941642d974685d288aed62f7b8d51f2d56
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-10-25 16:34:44 +0530

    add:vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ThemeProvider } from './components/ui/theme-provider.tsx'
-import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import store from './context/store.ts'
-import { SocketContextProvider } from './context/socket-context/socketContext.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/ui/theme-provider.tsx";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./context/store.ts";
+import { SocketContextProvider } from "./context/socket-context/socketContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <SocketContextProvider>
-        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Analytics />
           <App />
-          <Toaster position='top-center' reverseOrder={false} />
+          <Toaster position="top-center" reverseOrder={false} />
         </ThemeProvider>
       </SocketContextProvider>
     </Provider>
   </StrictMode>
-)
+);
